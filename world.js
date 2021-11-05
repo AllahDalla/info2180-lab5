45bad807fa71d734ae88e6f2e23a26586db0e5c3
AllahDalla <alromariodavis@gmail.com>
2021-11-05 03:31:46 -0400

    All functionalities are working - Styling required

--- world.js
+++ world.js
@@ -5,7 +5,7 @@ window.onload = function() {
 }
 
 function buttonFunction(){
-	
+
 	var btn = document.getElementById("lookup")
 	btn.addEventListener("click" , async function(e){
 
@@ -13,7 +13,7 @@ function buttonFunction(){
 
 		var input = document.getElementById("country").value
 		var results = document.getElementById("result")
-		var url = "world.php?country="+input
+		var url = "world.php?country="+input+"&context="
 
 		results.innerHTML = "";
 
@@ -24,7 +24,7 @@ function buttonFunction(){
 					let data = await response.text()
 					
 					results.innerHTML = ""+data;
-					return console.log(data);
+					return;
 
 				}else{
 					return Promise.reject("Response was not 200")
@@ -34,6 +34,37 @@ function buttonFunction(){
 				console.log("An error occured with the connection. Error is : "+ error)
 			})
 
+
+
+	})
+
+	var btn_cities = document.getElementById("lookup-cities")
+	btn_cities.addEventListener("click", async function(f){
+
+		f.target.preventDefault;
+
+		var input = document.getElementById("country").value
+		var results = document.getElementById("result")
+		var url = "world.php?country="+input+"&context=cities"
+
+		results.innerHTML = "";
+
+
+		await fetch(url)
+			.then(async response => {
+				if(response.ok) {
+					let data = await response.text()
+					
+					results.innerHTML = ""+data;
+					return;
+
+				}else{
+					return Promise.reject("Response was not 200");
+				}
+			})
+			.catch(error => {
+				console.log("An error occured with the connection. Error is : "+ error)
+			})
 
 
 	})
--- world.php
+++ world.php
@@ -4,36 +4,59 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'];
+$context = $_GET['context'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+if ($context == "cities"){
+	$stmt = $conn->query("SELECT countries.name, cities.name, cities.district, cities.population
+							FROM countries
+							JOIN cities
+							ON countries.code=cities.country_code
+							WHERE countries.name LIKE '%$country%';");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	?>
+	<table>
+		<tr>
+			<th>NAME</th>
+			<th>DISTRICT</th>
+			<th>POPULATION</th>
+		</tr>
+	<?php foreach ($results as $row): ?>
+		<tr>
+			<td><?= $row['name'];?></td>
+			<td><?= $row['district'];?></td>
+			<td><?= $row['population'];?></td>
+		</tr>
+	<?php endforeach; ?>
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	<?php 
+
+}else {
+	$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	?>
+
+
+	<table>
+		<tr>
+			<th>NAME</th>
+			<th>CONTINENT</th>
+			<th>INDEPENDENCE</th>
+			<th>HEAD OF STATE</th>
+		</tr>
+	<?php foreach ($results as $row): ?>
+		<tr>
+			<td><?= $row['name'];?></td>
+			<td><?= $row['continent'];?></td>
+			<td><?= $row['independence_year'];?></td>
+			<td><?= $row['head_of_state'];?></td>
+		</tr>
+	<?php endforeach; ?>
+	</table>
+	<?php 
+}
+
 
 ?>
-<!-- <ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul> -->
-
-<table>
-	<tr>
-		<th>NAME</th>
-		<th>CONTINENT</th>
-		<th>INDEPENDENCE</th>
-		<th>HEAD OF STATE</th>
-	</tr>
-<?php foreach ($results as $row): ?>
-	<tr>
-		<td><?= $row['name'];?></td>
-		<td><?= $row['continent'];?></td>
-		<td><?= $row['independence_year'];?></td>
-		<td><?= $row['head_of_state'];?></td>
-	</tr>
-<?php endforeach; ?>
 
 
-
-
-</table>
